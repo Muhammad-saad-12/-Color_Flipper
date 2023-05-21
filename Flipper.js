2273b90297bde2d76a3fe2523bbf80c0d6739ff1
Muhammad <saadazhar932@gmail.com>
2023-05-22 01:11:58 +0500

    Added some colours

--- Flipper.js
+++ Flipper.js
@@ -1,12 +1,12 @@
-const colors=["green","red","rgba(133,122,200)","#f15025"];
+const colors=["green","red","rgba(133,122,200)","#f15025","hotpink","lightblue","aqua"];
 const btn=document.getElementById('btn');
-   const color =document.querySelector(".color");//confusion.
+   const color =document.querySelector(".color");
 btn.addEventListener('click',function(){
     //Get random b/w 0-3.
 const randomNumber=getrandomNumber();
 document.body.style.backgroundColor =colors[randomNumber];
 color.textContent=colors[randomNumber];
-})
+});
 function getrandomNumber(){
 return Math.floor(Math.random()*colors.length);
 }
